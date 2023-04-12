69f0317272a373e804ab81d965e95be6b716a0c2
mjoshva <joshvav1@gmail.com>
2023-04-12 19:10:38 +0530

    first commit

---
+++ src/test/java/com/example/springjenkinsdockerdemo/SpringJenkinsDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjenkinsdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
