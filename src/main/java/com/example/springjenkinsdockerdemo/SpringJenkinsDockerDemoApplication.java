ea974e38f5b1d3aa2e83d2ae3c68f79fd0df7583
mjoshva <joshvav1@gmail.com>
2023-04-12 20:30:49 +0530

    second commit

--- src/main/java/com/example/springjenkinsdockerdemo/SpringJenkinsDockerDemoApplication.java
+++ src/main/java/com/example/springjenkinsdockerdemo/SpringJenkinsDockerDemoApplication.java
@@ -2,10 +2,18 @@ package com.example.springjenkinsdockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinsDockerDemoApplication {
 
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Docker";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsDockerDemoApplication.class, args);
 	}
